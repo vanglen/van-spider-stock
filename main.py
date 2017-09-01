b87a45d6e981a5c010115d54c7ce4b494b0ce2de
Vanglen <Vanglen.h@gmail.com>
2017-09-01 20:09:39 +0800

    up

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import pymysql.cursors
-import urllib
 import urllib2
 
 
-class _Request():
+class _DbProvider():
     def __init__(self):
         pass
 
@@ -35,6 +34,36 @@ class _Request():
         finally:
             connection.close()
 
+    def addStockData(self, data):
+        # Connect to the database
+        connection = pymysql.connect(host='localhost',
+                                     user='root',
+                                     password='root',
+                                     db='test',
+                                     charset='utf8',
+                                     cursorclass=pymysql.cursors.DictCursor)
+
+        try:
+            lastid = 0
+            with connection.cursor() as cursor:
+                # Create a new record
+                sql = "INSERT INTO `stockdata` (`datas`) VALUES (%s)"
+                cursor.execute(sql, (data,))
+                lastid = cursor.lastrowid
+
+            # connection is not autocommit by default. So you must commit to save
+            # your changes.
+            connection.commit()
+
+            with connection.cursor() as cursor:
+                # Read a single record
+                sql = "SELECT `id`, `datas` FROM `stockdata` WHERE `id`=%s"
+                cursor.execute(sql, (lastid,))
+                result = cursor.fetchone()
+                print(result)
+        finally:
+            connection.close()
+
 
 class _Request():
     def __init__(self):
@@ -43,10 +72,15 @@ class _Request():
     def test(self):
         requrl = "http://proxy.finance.qq.com/ifzqgtimg/appstock/app/HsDealinfo/getDadan?code=sz002036&_appName=android&_dev=Redmi+Note+4X&_devId=c50cb922972722424434833f4167e6721c90d991&_mid=c50cb922972722424434833f4167e6721c90d991&_md5mid=22523BE796DEE7EA0C09F1346EF28481&_omgid=d60a9565b0d6064e612955b535047ef779d10010212615&_omgbizid=096de883a6ae2d4b6f395e484189e416b9ba0140212718&_appver=5.8.2&_ifChId=119&_screenW=1080&_screenH=1920&_osVer=6.0.1&_uin=10000&_wxuin=20000&_net=WIFI&__random_suffix=38315"
         req = urllib2.Request(url=requrl)
-        print req
         res_data = urllib2.urlopen(req)
         res = res_data.read()
-        print res
+        return res
 
-req =_Request()
-req.test()
+
+req = _Request()
+apiresult = req.test()
+print apiresult
+
+_db = _DbProvider()
+dbresult = _db.addStockData(apiresult)
+print dbresult
