7a713722f648f39c5c1632fbec63b41c49d1b6ab
VANGLEN-PC\Vanglen <vanglen.h@gmail.com>
2017-09-03 01:54:00 +0800

    up

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import pymysql.cursors
 import urllib2
 
 
-class _DbProvider():
+class StockSpider():
     def __init__(self):
         pass
 
-    def test(self):
+    def addTest(self):
         # Connect to the database
         connection = pymysql.connect(host='localhost',
                                      user='root',
                                      password='root',
-                                     db='test',
+                                     db='van_spider',
                                      charset='utf8',
                                      cursorclass=pymysql.cursors.DictCursor)
 
@@ -39,7 +39,7 @@ class _DbProvider():
         connection = pymysql.connect(host='localhost',
                                      user='root',
                                      password='root',
-                                     db='test',
+                                     db='van_spider',
                                      charset='utf8',
                                      cursorclass=pymysql.cursors.DictCursor)
 
@@ -60,16 +60,11 @@ class _DbProvider():
                 sql = "SELECT `id`, `datas` FROM `stockdata` WHERE `id`=%s"
                 cursor.execute(sql, (lastid,))
                 result = cursor.fetchone()
-                print(result)
+                return result
         finally:
             connection.close()
 
-
-class _Request():
-    def __init__(self):
-        pass
-
-    def test(self):
+    def getApiData(self):
         requrl = "http://proxy.finance.qq.com/ifzqgtimg/appstock/app/HsDealinfo/getDadan?code=sz002036&_appName=android&_dev=Redmi+Note+4X&_devId=c50cb922972722424434833f4167e6721c90d991&_mid=c50cb922972722424434833f4167e6721c90d991&_md5mid=22523BE796DEE7EA0C09F1346EF28481&_omgid=d60a9565b0d6064e612955b535047ef779d10010212615&_omgbizid=096de883a6ae2d4b6f395e484189e416b9ba0140212718&_appver=5.8.2&_ifChId=119&_screenW=1080&_screenH=1920&_osVer=6.0.1&_uin=10000&_wxuin=20000&_net=WIFI&__random_suffix=38315"
         req = urllib2.Request(url=requrl)
         res_data = urllib2.urlopen(req)
@@ -77,10 +72,9 @@ class _Request():
         return res
 
 
-req = _Request()
-apiresult = req.test()
+spider = StockSpider()
+apiresult = spider.getApiData()
 print apiresult
 
-_db = _DbProvider()
-dbresult = _db.addStockData(apiresult)
+dbresult = spider.addStockData(apiresult)
 print dbresult
