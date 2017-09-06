6cfd048bf70d8eab75851f40c918687f4bea3c05
Vanglen <Vanglen.h@gmail.com>
2017-09-06 18:21:57 +0800

    up

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
+# -*- coding: utf-8 -*
 import pymysql.cursors
 import urllib2
+import time
+import json
 
 
 class StockSpider():
     def __init__(self):
         pass
+
+    def getDateTimeNow(self):
+        return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+
+    def getDateToday(self):
+        return time.strftime('%Y-%m-%d', time.localtime(time.time()))
+
+    def getTimeNow(self):
+        return time.strftime('%H:%M:%S', time.localtime(time.time()))
+
+    def getConn(self):
+        connection = pymysql.connect(host='localhost',
+                                     user='root',
+                                     password='root',
+                                     db='van_spider',
+                                     charset='utf8',
+                                     cursorclass=pymysql.cursors.DictCursor)
+        return connection
 
     def addTest(self):
         # Connect to the database
@@ -36,12 +57,7 @@ class StockSpider():
 
     def addStockData(self, data):
         # Connect to the database
-        connection = pymysql.connect(host='localhost',
-                                     user='root',
-                                     password='root',
-                                     db='van_spider',
-                                     charset='utf8',
-                                     cursorclass=pymysql.cursors.DictCursor)
+        connection = self.getConn()
 
         try:
             lastid = 0
@@ -64,17 +80,50 @@ class StockSpider():
         finally:
             connection.close()
 
+    # data format:["14:59:56", "11.17", "4601", "S"],
+    def addStockOrder(self, data):
+
+        # Connect to the database
+        connection = self.getConn()
+
+        try:
+            lastid = 0
+            with connection.cursor() as cursor:
+                # Create a new record
+                sql = "INSERT INTO `van_spider`.`t_stock_order`(`order_time`,`price`,`total_price`,`num`,`type`,`create_time`)VALUES (%s,%s,%s,%s,%s,%s)"
+
+                cursor.execute(sql, ("{0:s} {1:s}".format(self.getDateToday(), data[0]), data[1], 0, data[2], data[3],
+                                     self.getDateTimeNow()))
+                lastid = cursor.lastrowid
+
+            # connection is not autocommit by default. So you must commit to save
+            # your changes.
+            connection.commit()
+
+            with connection.cursor() as cursor:
+                # Read a single record
+                sql = "SELECT `id`, `datas` FROM `stockdata` WHERE `id`=%s"
+                cursor.execute(sql, (lastid,))
+                result = cursor.fetchone()
+                return result
+        finally:
+            connection.close()
+
     def getApiData(self):
-        requrl = "http://proxy.finance.qq.com/ifzqgtimg/appstock/app/HsDealinfo/getDadan?code=sz002036&_appName=android&_dev=Redmi+Note+4X&_devId=c50cb922972722424434833f4167e6721c90d991&_mid=c50cb922972722424434833f4167e6721c90d991&_md5mid=22523BE796DEE7EA0C09F1346EF28481&_omgid=d60a9565b0d6064e612955b535047ef779d10010212615&_omgbizid=096de883a6ae2d4b6f395e484189e416b9ba0140212718&_appver=5.8.2&_ifChId=119&_screenW=1080&_screenH=1920&_osVer=6.0.1&_uin=10000&_wxuin=20000&_net=WIFI&__random_suffix=38315"
+        requrl = "http://proxy.finance.qq.com/ifzqgtimg/appstock/app/HsDealinfo/getDadan?code=sz300672&_appName=android&_dev=Redmi+Note+4X&_devId=c50cb922972722424434833f4167e6721c90d991&_mid=c50cb922972722424434833f4167e6721c90d991&_md5mid=22523BE796DEE7EA0C09F1346EF28481&_omgid=d60a9565b0d6064e612955b535047ef779d10010212615&_omgbizid=096de883a6ae2d4b6f395e484189e416b9ba0140212718&_appver=5.8.2&_ifChId=119&_screenW=1080&_screenH=1920&_osVer=6.0.1&_uin=10000&_wxuin=20000&_net=WIFI&__random_suffix=38315"
         req = urllib2.Request(url=requrl)
         res_data = urllib2.urlopen(req)
         res = res_data.read()
         return res
 
-
 spider = StockSpider()
 apiresult = spider.getApiData()
 print apiresult
 
-dbresult = spider.addStockData(apiresult)
-print dbresult
+jsondata = json.loads(apiresult)
+if jsondata["code"] == 0:
+    detaillist = jsondata["data"]["detail"]
+    if len(detaillist) > 0:
+        for detail in detaillist:
+            dbresult = spider.addStockOrder(detail)
+            # print dbresult
--- script/van_spider.sql
+++ script/van_spider.sql
@@ -13,3 +13,13 @@ CREATE TABLE `stockdata` (
   `datas` varchar(4000) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+CREATE TABLE `t_stock_order` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `order_time` datetime DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `total_price` decimal(10,2) DEFAULT NULL,
+  `num` int(11) DEFAULT NULL,
+  `type` varchar(45) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
